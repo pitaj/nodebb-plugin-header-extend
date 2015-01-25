d5da3ed972e153dec7c4987898415bc8fe424a1b
Peter Jaszkowiak <p.jaszkow@gmail.com>
2015-01-25 00:02:46 -0700

    0.1.2

--- public/header-extend.js
+++ public/header-extend.js
@@ -1,21 +1,10 @@
 "use strict";
 
 $(document).ready(function(){
-
-  var thefunc = function () {
-    $(this).parent().toggleClass("expand");
-  }, as = $(".header-extend-menu > a:first-child").off().click(thefunc);
-
-  function reset(){
-    as.off().click(thefunc);
-  }
-
-  setTimeout(reset, 200);
-  setTimeout(reset, 400);
-  setTimeout(reset, 600);
-  setTimeout(reset, 800);
-  setTimeout(reset, 1000);
-
   $(".header-extend-item span[target=_blank]").parent().parent().attr("target", "_blank");
-
+  $(window).on('action:ajaxify.end', function(event, data) {
+    $(".header-extend-menu > a:first-child").off().click(function () {
+      $(this).parent().toggleClass("expand");
+    });
+  });
 });
